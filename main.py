2f2941baa9d4a57a0c14bd6e5b13e523c0939c21
grzesiekdev <grzesiekb@onet.pl>
2023-10-18 16:09:09 +0200

    added quiz flow wihout validating answers

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def delete_question(question_id: int, db: Session = Depends(database.get_db)):
 # Routes for managing sets of questions
 
 
-@app.post("/sets-of-questions/", response_model=schemas.SetOfQuestions)
+@app.post("/set-of-questions/", response_model=schemas.SetOfQuestions)
 def create_set_of_questions(set_of_questions: schemas.SetOfQuestionsCreate, db: Session = Depends(database.get_db)):
     return crud.create_set_of_questions(db=db, set_of_questions=set_of_questions)
 
@@ -78,21 +78,21 @@ def read_sets_of_questions(skip: int = 0, limit: int = 100, db: Session = Depend
     return sets_of_questions
 
 
-@app.get("/sets-of-questions/{set_id}", response_model=schemas.SetOfQuestions)
-def read_set_of_questions(set_id: int, db: Session = Depends(database.get_db)):
+@app.get("/set-of-questions/{set_id}", response_class=HTMLResponse)
+def read_set_of_questions(set_id: int, request: Request, db: Session = Depends(database.get_db)):
     db_set_of_questions = crud.get_set_of_questions(db, set_id=set_id)
     if db_set_of_questions is None:
         raise HTTPException(status_code=404, detail="Set of questions not found")
-    return db_set_of_questions
+    return templates.TemplateResponse("panel/quiz/quiz.html", {'request': request, 'set_of_questions': db_set_of_questions})
 
 
-@app.get("/sets-of-questions/{set_id}/questions/", response_model=list[schemas.Question])
+@app.get("/set-of-questions/{set_id}/questions/", response_model=list[schemas.Question])
 def read_questions_in_set(set_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
     questions = crud.get_questions_in_set(db, set_id, skip=skip, limit=limit)
     return questions
 
 
-@app.post("/sets-of-questions/{set_id}/questions/", response_model=schemas.Question)
+@app.post("/set-of-questions/{set_id}/questions/", response_model=schemas.Question)
 def create_question_in_set(set_id: int, question: schemas.QuestionCreate, db: Session = Depends(database.get_db)):
     return crud.create_question_in_set(db=db, question=question, set_id=set_id)
 
--- static/assets/js/api/setsofquestions.js
+++ static/assets/js/api/setsofquestions.js
@@ -8,18 +8,18 @@ function getSetsOfQuestions(url) {
         });
 }
 
-function createSetCard(name, description) {
+function createSetCard(name, description, id) {
     // Create the card structure using jQuery
     const card = $('<div class="col-md-3 my-2">')
         .append($('<div class="card-sl">')
             .append($('<div class="card-image">')
                 .append('<img src="https://altc.alt.ac.uk/blog/wp-content/uploads/sites/1112/2022/08/Blog-Cover-Guidlines-707x409.png">')
             )
-            .append('<a class="card-action" href="#"><i class="fa fa-heart"></i></a>')
+            .append('<a class="card-action"><i class="fa fa-heart"></i></a>')
             .append($('<div class="card-heading set-title">').text(name))
             .append($('<div class="card-text set-description">').text(description))
             .append('<div class="card-text set-date"><!-- TODO: Date will be here... --></div>')
-            .append('<a href="#" class="card-button">Take the quiz</a>')
+            .append('<a href="/set-of-questions/' + id + '" class="card-button">Take the quiz</a>')
         );
 
     // Append the card to the parent container
@@ -30,7 +30,7 @@ function handleSetsOfQuestions(){
 getSetsOfQuestions('/sets-of-questions/')
     .then(data => {
         data.forEach(set => {
-            createSetCard(set.name, set.description);
+            createSetCard(set.name, set.description, set.id);
         });
     })
     .catch(error => {
@@ -38,5 +38,51 @@ getSetsOfQuestions('/sets-of-questions/')
     });
 }
 
+function quizFlow(){
+    let currentQuestionIndex = 0;
+    const $questionContainers = $('.question');
+    const $nextButton = $('.next-question-button');
+    const $finishButton = $('.finish-button');
 
-export {handleSetsOfQuestions};
+    function showQuestion(index) {
+        $questionContainers.attr('hidden', true);
+        $questionContainers.eq(index).removeAttr('hidden');
+
+        if (index === 0) {
+            $('.previous-question-button').attr('hidden', true);
+        } else {
+            $('.previous-question-button').removeAttr('hidden');
+        }
+
+        if (index === $questionContainers.length - 1) {
+            $nextButton.hide();
+            $finishButton.removeAttr('hidden');
+        } else {
+            $nextButton.show();
+            $finishButton.attr('hidden', true);
+        }
+    }
+
+    showQuestion(currentQuestionIndex);
+
+    $nextButton.on('click', function () {
+        if (currentQuestionIndex < $questionContainers.length - 1) {
+            currentQuestionIndex++;
+            showQuestion(currentQuestionIndex);
+        }
+    });
+
+    $finishButton.on('click', function () {
+        // Handle finish/submit action here
+    });
+
+    $('.previous-question-button').on('click', function () {
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--;
+            showQuestion(currentQuestionIndex);
+        }
+    });
+}
+
+
+export {handleSetsOfQuestions, quizFlow};
